98f83f0573ec7b2bc0cc3a397a3ed10085cbd367
Walt Deyzel <waltdeyzel3141@gmail.com>
2021-01-30 22:25:48 +0200

    Simulated multiple population sizes and mutation rates.

--- version2/Simulate.py
+++ version2/Simulate.py
@@ -2,24 +2,31 @@ from Genome import Genome
 import operator
 import time
 from numpy import random as npR
-from Breeding import *
+from Breeding import generate, pick, crossover2, shuffle, crossover
+import matplotlib.pyplot as plt
+import math
 
 class Simulate:
 
-    def __init__(self, number_of_points, population_total, mutation_rate, solution):
+    def __init__(self, number_of_points, population_total, mutation_rate, solution, generations):
         self.number_of_points = number_of_points
         self.population_total = population_total
         self.mutation_rate = mutation_rate
         self.solution = solution
         self.population = []
+        self.generations = generations
+
+        self.results = []
     
     def run(self):
         for _ in range(self.population_total):
             scramble = shuffle(self.solution)
             self.population.append(Genome(scramble))
     
-        for i in range(1000):
-            print('GENERATION :', i, round(1/self.population[0].getFitness()))
+        for _ in range(self.generations):
+            best_fitness = 1/math.sqrt(self.population[0].getFitness())
+            if best_fitness > 1:
+                self.results.append(best_fitness)
             population_fitness = 0
             
             for genome in self.population:
@@ -32,7 +39,6 @@ class Simulate:
             sorted_population = sorted(self.population, key=operator.attrgetter('fitness_ratio'))
             sorted_population.reverse()
             self.population.clear()
-            sorted_population[0].draw(sorted_population[0].getSolution())
             self.population.append(sorted_population[0])
             self.population.append(sorted_population[1])
 
@@ -40,15 +46,47 @@ class Simulate:
 
                 option_1 = pick(sorted_population)
                 option_2 = pick(sorted_population)
-                new_genome = Genome(crossover2(option_1, option_2))
+                new_genome = Genome(crossover(option_1, option_2))
                 if npR.uniform() < self.mutation_rate:
                     new_genome.mutate()
                 self.population.append(new_genome)
+        self.population[0].draw(self.population[0].getSolution())
+        return self.results
 
 
 
 if __name__ == "__main__":
 
-    solution = generate(5)
-    test_1 = Simulate(len(solution), 3, 0.1, solution)
-    test_1.run()
+    simulations = 10000
+    # population_size = 30
+    path = generate(100)
+    # sim_1 = Simulate(len(path), population_size, 0.1, path, simulations)
+    # sim_2 = Simulate(len(path), population_size, 0.15, path, simulations)
+    # sim_3 = Simulate(len(path), population_size, 0.2, path, simulations)
+    # sim_4 = Simulate(len(path), population_size, 0.25, path, simulations)
+    # sim_5 = Simulate(len(path), population_size, 0.3, path, simulations)
+    # sim_6 = Simulate(len(path), population_size, 0.7, path, simulations)
+
+    # plt.plot(sim_1.run()) #BLUE
+    # plt.plot(sim_2.run()) #ORANGE
+    # plt.plot(sim_3.run()) #GREEN
+    # plt.plot(sim_4.run()) #RED
+    # plt.plot(sim_5.run()) #PURPLE
+    # plt.plot(sim_6.run()) #BROWN
+    # plt.show()
+   
+    mutation_rate = 0.2
+    sim_1 = Simulate(len(path), 5, mutation_rate, path, simulations)
+    sim_2 = Simulate(len(path), 10, mutation_rate, path, simulations)
+    sim_3 = Simulate(len(path), 20, mutation_rate, path, simulations)
+    sim_4 = Simulate(len(path), 30, mutation_rate, path, simulations)
+    sim_5 = Simulate(len(path), 40, mutation_rate, path, simulations)
+    sim_6 = Simulate(len(path), 70, mutation_rate, path, simulations)
+
+    plt.plot(sim_1.run()) #BLUE
+    plt.plot(sim_2.run()) #ORANGE
+    plt.plot(sim_3.run()) #GREEN
+    plt.plot(sim_4.run()) #RED
+    plt.plot(sim_5.run()) #PURPLE
+    plt.plot(sim_6.run()) #BROWN
+    plt.show()
