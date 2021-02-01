ac7d5328a7ca3b933d63d023796426bb85ca4d46
Walt Deyzel <waltdeyzel3141@gmail.com>
2021-02-01 18:43:56 +0200

    Added comments.

--- version2/Simulate.py
+++ version2/Simulate.py
@@ -22,35 +22,40 @@ class Simulate:
         self.results = []
     
     def run(self):
+        # CREATE POPULATION WITH GIVEN PATH
         for _ in range(self.population_total):
             scramble = shuffle(self.solution)
             self.population.append(Genome(scramble))
+        # KEEP TRACK OF THE BEST GENOME IN GIVEN POPULATION. FIRST ITTERATION IS RANDOM.    
         best_genome = self.population[0]
+        # RUN AMOUNT OF SIMULATIONS/GENERATIONS SPESIFIED
         for _ in range(self.generations):
-            
+            # KEEP TRACK OF THE SUM OF ALL THE POPULATION FITNESS
             population_fitness = 0
             for genome in self.population:
                 genome.fitness()
                 population_fitness += genome.getFitness()
-                
+            # NORMALISE THE FITNESS OF EACH GENOME IN THE POPULATION --- ONLY USED FOR ROULETTE SELECTION
             for genome in self.population:
                 genome.setFitness2Population(population_fitness)
-
+            # KEEP A COPY OF THE POPULATION
             sorted_population = self.population.copy()
+            # KEEP TRACK OF THE FITTEST GENOME IN THE POPULATION
             best_genome = max(self.population, key=operator.attrgetter('fit'))
-            #print(1/math.sqrt(best_genome.getFitness()))
-            best_fitness = 1/math.sqrt(best_genome.getFitness()) 
+            best_fitness = 1/math.sqrt(best_genome.getFitness())
+            # ADD THE BEST FITNESS SCORE TO A LIST  
             if best_fitness > 1:
                 self.results.append(best_fitness)
-    
+            # CLEAR POPULATION TO FILL WITH A NEW GENERATION OF GENOMES
             self.population.clear()
+            # COPY OVER THE BEST SOLUTION FROM THE PREVIOUS GENERATION --  THE COPY
             self.population.append(best_genome)
             self.population.append(best_genome)
-            
+            # FILL THE REST OF EMPTY POPULATION USING THE SELECTION ALG OF CHOICE.
             while len(self.population) != self.population_total:
                 
                 new_genome = self.selection(sorted_population)
-
+                # CROSSOVER RATE
                 if npR.uniform() < self.crossover_rate:
                     option_1 = new_genome
                     option_2 = self.selection(sorted_population)
@@ -59,9 +64,10 @@ class Simulate:
                         new_genome = Genome(self.crossover(option_1, option_2, option_3))
                     else:
                         new_genome = Genome(self.crossover(option_1, option_2))
-
+                # MUTATION RATE
                 if npR.uniform() < self.mutation_rate:
                     new_genome.mutate()
+                # ADD NEWEST MEMBER TO POPULATION
                 self.population.append(new_genome)
 
         best_genome.draw(best_genome.getSolution())
@@ -72,8 +78,8 @@ class Simulate:
 
 if __name__ == "__main__":
 
-    simulations = 30000
-    crossover_rate =1
+    simulations = 10000
+    crossover_rate = 0.7
     mutation_rate = 0.25
     population_size = 30
     path_len = 50
